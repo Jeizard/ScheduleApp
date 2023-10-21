823e43bf698b0461d025feb69d84a8ca57048fb9
Jeizard <japhex@mail.ru>
2023-10-21 17:46:09 +0300

    Initial commit +Creating Modules according to a Clean Architecture +Connecting the Room library

---
+++ app/src/main/java/com/jeizard/scheduleapp/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jeizard.scheduleapp.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jeizard.scheduleapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
